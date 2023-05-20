3e8cec76332bc46a5c4643046cef3842ead7195e
bilimerdi <orunerdi186@gmail.com>
2023-05-20 23:42:19 +0300

    Update RegistryForm.js
    registryForm.js dosyasında stiller düzeltildi.

--- src/components/RegistryForm.js
+++ src/components/RegistryForm.js
@@ -1,43 +1,105 @@
-import { FormControl, Button, TextField } from "@mui/material";
+import {
+  FormControl,
+  Button,
+  TextField,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
+import "./RegistryForm.css";
 
 const RegistryForm = () => {
   const [fullName, setFullName] = useState();
   const [tc, setTc] = useState();
   const [assistant, setAssistant] = useState();
+  const [detail, setDetail] = useState();
+  const [diagnosis, setDiagnosis] = useState();
   return (
-    <div style={{ backgroundColor: "#faedcd" }}>
+    <Stack
+      sx={{
+        backgroundColor: "#d4a373",
+        height: 450,
+        width: 500,
+        textAlign: "left",
+        margin: "auto",
+        marginTop: 10,
+        border: 1,
+        borderColor: "black",
+        padding: 5,
+        borderRadius: "2%",
+      }}
+    >
       <FormControl>
+        <Typography align="center" variant="h5">
+          Hasta Kayıt Formu
+        </Typography>
         <TextField
           label="Laborant Ad Soyad:"
-          variant="outlined"
-          sx={{ marginBottom: 2 }}
-          color="secondary"
+          id="MuiInputBase-input"
+          variant="filled"
+          color="success"
+          sx={{ marginBottom: 2, marginTop: 2, width: 500 }}
           onChange={(e) => setAssistant(e.target.value)}
           value={assistant}
           required
         ></TextField>
+        <Stack direction="row">
+          <TextField
+            label="Hasta Ad Soyad:"
+            variant="filled"
+            color="success"
+            sx={{ marginBottom: 2, width: 248, backgroundColor: "#faedcd" }}
+            onChange={(e) => setFullName(e.target.value)}
+            value={fullName}
+            required
+          ></TextField>
+          <TextField
+            label="TC No"
+            variant="filled"
+            color="success"
+            sx={{ marginBottom: 2, marginLeft: 2, width: 237 }}
+            onChange={(e) => setTc(e.target.value)}
+            value={tc}
+            required
+          ></TextField>
+        </Stack>
         <TextField
-          label="Hasta Ad Soyad:"
-          variant="outlined"
-          sx={{ marginBottom: 2 }}
-          color="secondary"
-          onChange={(e) => setFullName(e.target.value)}
-          value={fullName}
+          label="Tanı Başlığı"
+          variant="filled"
+          color="success"
+          sx={{ marginBottom: 2, width: 500 }}
+          onChange={(e) => setDiagnosis(e.target.value)}
+          value={diagnosis}
           required
         ></TextField>
+        <Typography sx={{ marginBottom: 2 }}>
+          Tanı detaylarını giriniz:
+        </Typography>
         <TextField
-          label="TC No"
-          variant="outlined"
-          sx={{ marginBottom: 2 }}
-          color="secondary"
-          onChange={(e) => setTc(e.target.value)}
-          value={tc}
+          multiline={true}
+          rows={3}
+          color="success"
+          sx={{ marginBottom: 2, width: 500, height: 100 }}
+          onChange={(e) => setDetail(e.target.value)}
+          value={detail}
           required
         ></TextField>
-        <Button type="submit">Kaydet</Button>
+        <Button
+          type="submit"
+          sx={{
+            marginBottom: 2,
+            width: 100,
+            backgroundColor: "#faedcd",
+            color: "black",
+            marginLeft: "auto",
+            marginRight: "auto",
+            display: "block",
+          }}
+        >
+          Kaydet
+        </Button>
       </FormControl>
-    </div>
+    </Stack>
   );
 };
 
