9644da6cef939cda2d7c090ba343315b866c21e1
bilimerdi <orunerdi186@gmail.com>
2023-05-21 01:13:11 +0300

    Created RegistryTable.js
    hastaların verilerini görüntülemek için material uı da datagrid komponenti kullanıldı.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import RegistryForm from "./components/RegistryForm";
+import RegistryTable from "./components/RegistryTable";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<RegistryForm></RegistryForm>}></Route>
+      <Route path="/" element={<RegistryTable></RegistryTable>}></Route>
+      <Route path="/Form" element={<RegistryForm></RegistryForm>}></Route>
     </Routes>
   );
 };
---
+++ src/components/RegistryTable.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import Icon1 from "../assets/hastane.jpg";
+import { DataGrid } from "@mui/x-data-grid";
+import { Button, Stack, SvgIcon } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+
+const openUser = (e, row) => {
+  return null;
+};
+
+const editUser = (e, row) => {
+  return null;
+};
+
+const deleteUser = (e, row) => {
+  return null;
+};
+
+const RegistryTable = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/user/getAll")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "Dosya No", width: 100 },
+    { field: "fullName", headerName: "Hasta Ad Soyad", width: 200 },
+    { field: "tc", headerName: "TC No", width: 150 },
+    { field: "diagnosis", headerName: "Tanı Başlığı", width: 200 },
+    { field: "assistant", headerName: "Laborant Ad Soyad", width: 200 },
+    {
+      field: "Edit",
+      headerName: "Edit",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <Stack direction="row">
+            <Button onClick={(e) => openUser(e, params.row)}>
+              <SvgIcon color="action" component={InfoIcon}></SvgIcon>
+            </Button>
+            <Button onClick={(e) => editUser(e, params.row)}>
+              <SvgIcon color="action" component={EditIcon}></SvgIcon>
+            </Button>
+            <Button onClick={(e) => deleteUser(e, params.row)}>
+              <SvgIcon color="action" component={DeleteIcon}></SvgIcon>
+            </Button>
+          </Stack>
+        );
+      },
+    },
+  ];
+  return (
+    <Stack>
+      <DataGrid columns={columns} rows={users} />
+    </Stack>
+  );
+};
+
+export default RegistryTable;
